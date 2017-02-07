c96faf54c17ab8d3d90069c35b3bdda4fbb09592
user <user@use1r-PC>
2017-02-07 18:08:39 +0800

    new demo

--- Architect/src/com/winterbe/java8/samples/stream/Streams9.java
+++ Architect/src/com/winterbe/java8/samples/stream/Streams9.java
@@ -2,20 +2,23 @@ package com.winterbe.java8.samples.stream;
 
 import java.util.Arrays;
 
-/**
- * @author Benjamin Winterberg
- */
+import org.junit.Test;
+
 public class Streams9 {
-
-    public static void main(String[] args) {
-        Arrays.asList("a1", "a2", "b1", "c2", "c1")
-            .stream()
-            .filter(s -> s.startsWith("c"))
-            .map(String::toUpperCase)
-            .sorted()
-            .forEach(System.out::println);
-
-        // C1
-        // C2
-    }
+	@Test
+	public void test0() {
+		Arrays.asList("apple", "ant", "banana", "pear", "cat", "dog")
+		.stream().filter(x -> (x.length() > 3))
+		.map(x -> x.toUpperCase()).sorted().forEach(System.out::println);
+	}
+	
+	@Test
+	public void test1() {
+		Arrays.asList("a1", "a2", "b1", "c2", "c1")
+		.stream()
+		.filter(s -> s.startsWith("c"))
+		.map(String::toUpperCase)
+		.sorted()
+		.forEach(System.out::println);
+	}
 }
